5c96ca1f76bf79da86624f207e3f1484c84e6062
ghost <ghost.vkv@gmail.com>
2022-03-26 18:11:00 +0300

    Add cors

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -46,6 +46,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:text, :is_completed, :project_id)
+      params.require(:todo).permit(:id, :text, :is_completed, :project_id)
     end
 end
